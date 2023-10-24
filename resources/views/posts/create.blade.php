70959b647a7906c64a4f7bb1d5cccd6ed0725651
SatoyaMaruyama <sappykinakochoco1012@gmail.com>
2023-10-24 19:21:02 +0900

    ブログ投稿保存処理

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -10,18 +10,18 @@
     </head>
     <body class="antialiased">
         <h1>Blog Name</h1>
-       <form action="/posts" method="POST">
-           @csrf
-           <div class="title">
-               <h2>Title</h2>
-               <input type="text" name=post[title] placeholder="タイトル">
-           </div>
-           <div class="body">
-               <h2>Body</h2>
-               <textarea name="post[body]" placeholder= "今日も一日お疲れ様でした。"></textarea>
-           </div>
-           <input type="submit" value="store">
-       </form>
+      <form action="/posts" method="POST">
+            @csrf
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" placeholder="タイトル"/>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
+            </div>
+            <input type="submit" value="store"/>
+        </form>
        <div class='footer'>
            <a href="/">戻る</a>
        </div>
